cf74f0c432bb16c7bd106e2626c2ca2d5f9d5b7c
wendellast <wendelalves01a@hotmail.com>
2024-05-18 22:13:44 -0300

    init: schema

---
+++ config/logger.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"io"
+	"log"
+	"os"
+)
+
+type Logger struct {
+	debug   *log.Logger
+	info    *log.Logger
+	warning *log.Logger
+	err     *log.Logger
+	writer  io.Writer
+}
+
+func NewLogger(prefix string) *Logger {
+	writer := io.Writer(os.Stdout)
+	logger := log.New(writer, prefix, log.Ldate|log.Ltime)
+
+	return &Logger{
+		debug:   log.New(writer, "DEBUG: ", logger.Flags()),
+		info:    log.New(writer, "INFO: ", logger.Flags()),
+		warning: log.New(writer, "WARNING: ", logger.Flags()),
+		err:     log.New(writer, "ERRO: ", logger.Flags()),
+		writer:  writer,
+	}
+}
--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "github.com/wendellast/Maybe/router"
+import (
+	"fmt"
+
+	"github.com/wendellast/Maybe/config"
+	"github.com/wendellast/Maybe/router"
+)
 
 func main() {
+	//Config Initializer
+	err := config.Init()
+
 	router.Initializer()
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
